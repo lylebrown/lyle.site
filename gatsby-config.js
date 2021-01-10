b118783af11666db22ab8dbf3c97ce5831798a1a
Lyle Brown <lyle@lylebrown.com>
2021-01-10 15:27:21 -0700

    metadata update and blogroll formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Lyle\'s Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A place for storing Lyle Brown\'s thoughts about technology',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
