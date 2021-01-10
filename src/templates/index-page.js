dcaa5a291cc1818a70df547cced6b0719c6d7ed0
Lyle Brown <lyle@lylebrown.com>
2021-01-10 13:13:13 -0700

    Remove header boxshadow

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,8 +39,7 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+
             
             color: 'white',
             lineHeight: '1',
@@ -52,8 +51,7 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+
             
             color: 'white',
             lineHeight: '1',
