77f6674e4c0e42d7b19ef738a9bb38295006b5f3
Lyle Brown <lyle@lylebrown.com>
2021-01-10 12:56:37 -0700

    Update nav and landing

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -41,7 +41,7 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -54,7 +54,7 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -88,11 +88,6 @@ export const IndexPageTemplate = ({
                 </div>
                 <Features gridItems={intro.blurbs} />
                 <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
